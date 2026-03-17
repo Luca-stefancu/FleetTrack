838ef428e6a578dc60afda2896546263eb07f7e6
Luca-stefancu <luca.stefancu@student.usv.ro>
2026-03-17 17:02:59 +0200

    Update Program.cs
    -actualizare program.cs
    
    -implementare lista vehicule
    
    -adaugare functionalitati: citire, afisare, cautare
    
    -implementare meniu si operatii pe lista

--- Program.cs
+++ Program.cs
@@ -5,14 +5,85 @@ namespace FleetTrack
     {
         static void Main()
         {
-            Vehicul v1 = new Vehicul("SV01ABC", "Volvo", "FH16", 20000);
-            Sofer s1 = new Sofer("Popescu", "Ion", "C+E", 8);
+            List<Vehicul> vehicule = new List<Vehicul>();
+            int optiune;
 
-            Cursa c1 = new Cursa("Suceava", "Cluj", 300, DateTime.Now, v1, s1);
+            do
+            {
+                Console.WriteLine("\n1. Adauga vehicul");
+                Console.WriteLine("2. Afiseaza vehicule");
+                Console.WriteLine("3.Cauta vehicul dupa numar");
+                Console.WriteLine("0. Iesire");
+                Console.Write("Alege optiunea: ");
 
-            Console.WriteLine($"Cursa: {c1.OrasPlecare} -> {c1.OrasDestinatie}");
-            Console.WriteLine($"Vehicul: {v1.Marca} {v1.Model}");
-            Console.WriteLine($"Sofer: {s1.Nume} {s1.Prenume}");
+                int.TryParse(Console.ReadLine(), out optiune);
+
+                switch (optine)
+                {
+                    case 1:
+                        AdaugaVehicul(vehicule);
+                        break;
+                    case 2:
+                        AfiseazaVehicule(vehicule);
+                        break;
+                    case 3:
+                        CautaVehicul(vehicule);
+                        break;
+
+                }
+            } while (optiune != 0);
+
         }
+
+        static void AdaugaVehicul(List<Vehicul> lista)
+        {
+            Console.Write("Numar inmatriculare: ");
+            string nr = Console.ReadLine();
+
+            Console.WriteLine("Marca: ");
+            string marca = Console.ReadLine();
+
+            Console.Write("Model: ");
+            string model = Console.ReadLine();
+
+            Console.Write("Capacitate incarcare: ");
+            double capacitate;
+            double.TryParse(Console.ReadLine(), out capacitate);
+
+            Vehicul v = new Vehicul(nr, marca, model, capacitate);
+            lista.Add(v);
+
+            Console.WriteLine("Vehicul adaugat.");
+
+            static void AfiseazaVehicule(List<Vehicul> lista)
+            {
+                if (lista.Count == 0)
+                {
+                    Console.WriteLine("Nu exista vehicule.");
+                    return;
+                }
+
+                foreach (var v in lista)
+                {
+                    Console.WriteLine($"{v.NumarInmatriculare} - {v.Marca} {v.Model} - {v.CapacitateIncarcare} kg");
+                }
+            }
+
+            static void CautaVehicul(List<Vehicul> lista)
+            {
+                Console.Write("Introdu numarul de inmatriculare: ");
+                string nr = Console.ReadLine();
+
+                foreach (var v in lista)
+                {
+                    if (v.NumarInmatriculare.Equals(nr, StringComparison.OrdinalIgnoreCase))
+                    {
+                        Console.WriteLine($"Gasit: {v.Marca} {v.Model}");
+                        return;
+                    }
+                }
+
+                Console.WriteLine("Vehiculul nu a fost gasit.");
+            }
     }
 }
